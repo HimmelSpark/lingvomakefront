b331fa4e021ed5b2527bb6c00ae270b6eec03880
HimmelSpark <adam404pet@gmail.com>
2019-03-12 13:54:49 +0300

    axios cookie fix4

--- src/network/http-common.js
+++ src/network/http-common.js
@@ -1,13 +1,10 @@
 import axios from 'axios'
 
-// axios.defaults.withCredentials(true);
-
 export const HTTP = axios.create({
   baseURL: 'http://localhost:8090',
   headers: {
-	'Access-Control-Allow-Origin': '*',
 	'Content-Type': 'application/json',
   },
 });
 
-// HTTP.defaults.withCredentials(true);
+HTTP.defaults.withCredentials = true;
