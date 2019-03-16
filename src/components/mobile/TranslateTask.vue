0d948a531ddd0ac7b5e34f32f117d32fac597441
HimmelSpark <adam404pet@gmail.com>
2019-03-16 13:05:02 +0300

    mobile translate view

--- src/components/mobile/TranslateTask.vue
+++ src/components/mobile/TranslateTask.vue
@@ -1,8 +1,35 @@
 <template>
-  <v-container>
-    <v-layout row>
+  <v-container fill-height>
+    <v-layout row wrap align-center>
       <v-flex xs12>
-        <h1>translate task</h1>
+        <v-card>
+          <v-card-text>
+            <div class="text-xs-center">
+              <v-chip color="green" text-color="white">{{text}}</v-chip>
+            </div>
+            <v-form v-model="valid" ref="form" validation>
+              <v-text-field
+                  xs6
+                  name="answer"
+                  label="write your answer here"
+                  type="text"
+                  :rules="answerRules"
+                  v-model="answer"
+              ></v-text-field>
+            </v-form>
+            <div class="text-xs-center">
+              <v-btn
+                  dark
+                  color="green"
+                  @click="onSubmit"
+              >Login</v-btn>
+            </div>
+          </v-card-text>
+        </v-card>
+
+
+
+
       </v-flex>
     </v-layout>
   </v-container>
@@ -12,7 +39,12 @@
   export default {
 	data() {
 	  return {
-
+      text: 'This is the text that you should translate',
+      valid: false,
+      answer: null,
+      answerRules: [
+      	v => !!v !== null
+      ]
 	  }
 	}
   }
