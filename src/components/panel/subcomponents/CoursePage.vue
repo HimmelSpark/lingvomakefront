281c1330e2e3ee2ee73de99f18b6cd65f9cea27e
HimmelSpark <adam404pet@gmail.com>
2019-03-18 10:16:05 +0300

    course delete modal dialog

--- src/components/panel/subcomponents/CoursePage.vue
+++ src/components/panel/subcomponents/CoursePage.vue
@@ -37,10 +37,22 @@
 
             <v-tooltip bottom>
               <template v-slot:activator="{on}">
-                <v-btn flat dark icon large v-on="on" color="red"><v-icon>delete</v-icon></v-btn>
+                <v-btn flat dark icon large v-on="on" color="red" @click="openDeleteDialog"><v-icon>delete</v-icon></v-btn>
               </template>
               <span>delete this COURSE</span>
             </v-tooltip>
+
+            <v-dialog v-model="deleteDialog" persistent max-width="290">
+              <v-card>
+                <v-card-title class="headline">Do you want to delete this course?</v-card-title>
+                <v-card-text>This process is irreversible, you can't restore this course later!</v-card-text>
+                <v-card-actions>
+                  <v-spacer></v-spacer>
+                  <v-btn color="primary" flat @click="deleteDialog = false">Cancel</v-btn>
+                  <v-btn color="red" flat @click="deleteDialog = false">Delete</v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-dialog>
 
           </v-card-actions>
 
@@ -52,18 +64,20 @@
 
 <script>
   export default {
-	data() {
-	  return {
-
-	  }
-	},
-	computed: {
-	  selected() {
-		return this.$store.getters.selected;
-	  },
-	},
-	methods: {
-
-	}
+    data() {
+      return {
+        deleteDialog: false
+      }
+    },
+    computed: {
+      selected() {
+      return this.$store.getters.selected;
+      },
+    },
+    methods: {
+      openDeleteDialog() {
+        this.deleteDialog = true
+      }
+    }
   }
 </script>
